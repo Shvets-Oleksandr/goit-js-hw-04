f6170040bcdeffc9d8f3685be6e16fcc060c1506
Oleksandr Shvets <shvec.olx@gmail.com>
2024-06-30 18:41:07 +0200

    modyfi task-1,2,3

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,25 @@
 'use strict';
 
-function slugify(title) {
-    const normalizedTitle = title.toLowerCase();
-    const wordTitle = normalizedTitle.split(" ");
-    return wordTitle.join("-");
-}
+function isEnoughCapacity(products, containerSize) {
+    let sumProducts = 0;
+    for (const product in products) {
+        sumProducts += products[product];
+    }
+    return sumProducts <= containerSize;
+};
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
